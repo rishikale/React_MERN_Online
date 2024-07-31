fd98099ff8093ba5e10d5381b5e0d980d358e6d0
Meera Jadhav <mj202325@appystack.com>
2024-07-31 10:21:10 +0530

    reactHookForm hooks/functions

--- src/Components/ReactHookForm/ReactHookForm.js
+++ src/Components/ReactHookForm/ReactHookForm.js
@@ -12,14 +12,20 @@ function ReactHookForm() {
     age: 0,
   };
 
-  const { handleSubmit, register, reset } = useForm({
-    defaultValues: defaultValues,
-  });
+  const { handleSubmit, register, reset, watch, getValues, setValue } = useForm(
+    {
+      defaultValues: defaultValues,
+    }
+  );
 
   //   handleSubmit -- handleSubmit is the function or a code block which is used for getting all the values at a single submit .
   //   register -- used to register the local form fields with the react hook form that react hook form can interact with each single field .
+  //   watch -- watch is function used to get the field value before the submit targetting the field name
+  //            it is noting but just like the fields onChange event .
+  //   getValues -- getValues will work same as watch but it works onClick .
+  //   setValue -- it is the function used to set a forcefull value to any field lying between your form tag .
 
-  // a function used to get the form values when the button clicked which is having tye type "Submit"
+  // a function used to get the form values when the button clicked which is having tye type "Submit" .
   console.log("The values in the form :", users);
   function getFormValues(values) {
     let dataArray = [...users];
@@ -27,6 +33,48 @@ function ReactHookForm() {
     setUsers(dataArray);
     reset(defaultValues);
   }
+
+  // useCase of watch
+  // 1.
+  const firstNameWatch = watch("firstName");
+  // 2 .
+  const [firstName, middleName, lastName, age] = watch([
+    "firstName",
+    "middleName",
+    "lastName",
+    "age",
+  ]);
+
+  // useCase of getValues
+  function getFieldValues() {
+    let getValuesFirstName = getValues("firstName");
+
+
+    // useCase of setValue 
+    if(getValuesFirstName==="abc"){
+      setValue("age",20)
+      setValue("lastName","")
+    }else{
+      setValue("age",0)
+      setValue("lastName","last name")
+    }
+
+    // if(country==="india"){
+    //   setValue("isdCode",+91)
+    // }
+
+    console.log("getValues :", getValuesFirstName);
+  }
+
+  // useFieldArray() -- in tomorrows session
+
+  console.log(
+    "firstNameWatch value is :",
+    firstName,
+    middleName,
+    lastName,
+    age
+  );
 
   return (
     <form onSubmit={handleSubmit(getFormValues)}>
@@ -61,6 +109,14 @@ function ReactHookForm() {
           placeholder="Last Name"
         />
         <input name="age" {...register("age")} placeholder="Age" />
+        <button
+          type="button"
+          onClick={() => {
+            getFieldValues();
+          }}
+        >
+          Get Values
+        </button>
         <button type="submit">Submit</button>
       </div>
     </form>
